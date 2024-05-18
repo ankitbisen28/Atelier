c30056ebdd7b757e6db1c171ce103ad1ea53dcc4
Ankit <ankitbisen288@gmail.com>
2024-05-18 18:44:10 +0530

    MUI Removed

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import theme from "./theme";
-import { ThemeProvider } from "@mui/material/styles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
       <App />
-    </ThemeProvider>
   </React.StrictMode>
 );
