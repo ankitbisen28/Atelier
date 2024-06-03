e8635b7450d99e9ef2e7b52bbc6abf2e9748f079
Ankit <ankitbisen288@gmail.com>
2024-06-03 14:59:56 +0530

    inside repo name change

--- server/app.js
+++ server/app.js
@@ -8,8 +8,8 @@ const app = express();
 
 require('dotenv/config');
 
-const authJwt = require('./helpers/jwt');
-const errorHandler = require('./helpers/error-handler');
+const authJwt = require('./helpers/jwt.js');
+const errorHandler = require('./helpers/error-handler.js');
 
 app.use(cors());
 app.options('*', cors());
@@ -23,10 +23,10 @@ app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
 app.use(errorHandler);
 
 const api = process.env.API_URL;
-const categoriesRoute = require('./routes/categories');
+const categoriesRoute = require('./routes/categories.js');
 const projects = require('./routes/projects.js');
-const userRoute = require('./routes/users');
-const orderRoute = require('./routes/orders');
+const userRoute = require('./routes/users.js');
+const orderRoute = require('./routes/orders.js');
 
 // Routes
 app.use(`${api}/projects`, projects);
